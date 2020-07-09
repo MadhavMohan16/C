b819ac8a0a6ddd9523427fb9488b498660760a94
MadhavMohan16 <63821741+MadhavMohan16@users.noreply.github.com>
2020-07-09 23:19:47 +0530

    Create TernaryOperator2.c

---
+++ BasicProgram/TernaryOperator2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int N1,N2;
+	printf("Enter the Number");
+	scanf("%d%d",&N1,&N2);
+	(N1>N2)?printf("N1 is greater=%d",N1):printf("N2 is greater=%d",N2);
+	return 0;
+}
