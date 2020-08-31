a6c25794b117caba73a7c1672398f079d8e71a91
MadhavMohan16 <63821741+MadhavMohan16@users.noreply.github.com>
2020-09-01 00:04:02 +0530

    Transpose

---
+++ 2DimensionalArray4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],b[3][3],i,j;
+	printf("Enter the value in matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d  ",a[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			b[i][j]=a[j][i];
+		}
+	}
+	printf("Transponse of matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d  ",b[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
